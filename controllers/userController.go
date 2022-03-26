176175fb5a3197ef8fe438cfb53b0fb72e4a1e54
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-03-26 23:33:25 +0100

    started working on userController

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,1 +1,21 @@
-package controller
+package controllers
+
+import (
+	"golang-jwt-api-project/database"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+
+func HashPassword()
+
+func VerifyPassword()
+
+func Signup()
+
+func Login()
+
+func GetUsers()
+
+func GetUser()
