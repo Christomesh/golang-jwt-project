3eb4235cbccc3aa5db09b341a9191dcb59e0dece
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-03-28 09:48:55 +0100

    added two helper functions used in userController.go

--- helpers/authHelper.go
+++ helpers/authHelper.go
@@ -1,1 +1,32 @@
-package helpers
+package helper
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUserType(c *gin.Context, role string) (err error) {
+	userType := c.GetString("user_type")
+	err = nil
+	if userType != role {
+		err = errors.New("Unauthorized to access this resources")
+		return err
+	}
+	return err
+}
+
+func MatchUserTypeUid(c *gin.Context, userId string) (err error) {
+	userType := c.GetString("user_type")
+
+	uid := c.GetString("uid")
+
+	err = nil
+
+	if userType == "USER" && uid != userId {
+		err := errors.New("Unauthorized to acess this resources")
+		return err
+	}
+	err = CheckUserType(c, userType)
+	return err
+}
